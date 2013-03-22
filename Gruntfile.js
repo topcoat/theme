7296d1a5b0960221055685f793e8fea90e11cfd2
kristoferjoseph <kristoferjoseph@gmail.com>
2013-03-21 17:22:57 -0700

    Updating variable files to support both mobile and desktop. Removing unused node dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,52 +3,31 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        // TODO: Add git pull of skins defined in package.json topcoat.skins
-        // Task configuration.
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                globals: {}
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            lib_test: {
-                src: ['lib/**/*.js', 'test/**/*.js']
+        pkg: grunt.file.readJSON('package.json'),
+        topcoat: {
+            download: {
+                options: {
+                    srcPath: "src/",
+                    repos: "<%= pkg.topcoat %>"
+                }
             }
         },
-        nodeunit: {
-            files: ['test/**/*_test.js']
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'nodeunit']
+        unzip: {
+            skins: {
+                src: "src/skins/*.zip",
+                dest: "src/skins"
             }
+        },
+        gruntfile: {
+            src: 'Gruntfile.js'
         }
     });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-topcoat');
+    grunt.loadNpmTasks('grunt-zip');
 
     // Default task.
-    grunt.registerTask('default', ['jshint', 'nodeunit']);
+    grunt.registerTask('default', ['topcoat', 'unzip']);
 
 };
